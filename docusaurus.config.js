238f62a1eb3086335ca94daa2b6e2a319f37cf42
Eliott <ioiottdago@gmail.com>
2025-06-06 18:11:05 +0200

    Changed configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
 
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
