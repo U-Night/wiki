136e6e51f2d75cf333acdc1652c429be210b514b
ScreanDev <lucas.pausechapuis91@gmail.com>
2025-07-06 11:04:13 +0200

    Fixed incorrect links to team members' socials

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -7,10 +7,10 @@ export default function TeamMembers() {
         <div className="team-members">
             <TeamMember userName="NeyZzO" displayName="NeyZzO - Eliott" title="Developer" socials={{github: "https://github.com/NeyZzO"}}/>
             <TeamMember userName="ScreanDev" displayName="Screan - Lucas" title="Developer & Sound design" socials={{github: "https://github.com/ScreanDev"}}/>
-            <TeamMember userName="Rickolaa" displayName="Rickolaa - Pierrick" title="Developer" socials={{github: "https://github.com/NeyZzO"}}/>
-            <TeamMember userName="Soupexo" displayName="Soupe - Otman" title="Developer & Art" socials={{github: "https://github.com/NeyZzO"}}/>
-            <TeamMember userName="ddvur" displayName="ddvur - Liam" title="Developer" socials={{github: "https://github.com/NeyZzO"}}/>
-            <TeamMember userName="gabycretin" displayName="Gabycretin - Gabriel" title="Developer" socials={{github: "https://github.com/NeyZzO"}}/>
+            <TeamMember userName="Rickolaa" displayName="Rickolaa - Pierrick" title="Developer" socials={{github: "https://github.com/Rickolaa"}}/>
+            <TeamMember userName="Soupexo" displayName="Soupe - Otman" title="Developer & Art" socials={{github: "https://github.com/Soupexo"}}/>
+            <TeamMember userName="ddvur" displayName="ddvur - Liam" title="Developer" socials={{github: "https://github.com/ddvur"}}/>
+            <TeamMember userName="Gabycretin" displayName="Gabycretin - Gabriel" title="Developer" socials={{github: "https://github.com/Gabycretin"}}/>
         </div>
     )
 }
